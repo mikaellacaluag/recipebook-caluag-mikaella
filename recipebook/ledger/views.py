c3001fd11ec1382e1e4e3b28efa34dd665306432
mikaellacaluag <mikaella.caluag@student.ateneo.edu>
2026-02-09 12:47:24 +0800

    Fixed templates

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
     return HttpResponse('Welcome to the Ledger App!')
+
+def recipe_list(request):
+    ctx = [
+        
+    ]
+
+
 # Create your views here.
