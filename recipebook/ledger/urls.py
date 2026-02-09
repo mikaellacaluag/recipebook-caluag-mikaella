64665fdca49940bc434a83e2186a25e63877e50f
mikaellacaluag <mikaella.caluag@student.ateneo.edu>
2026-02-09 13:48:27 +0800

    Implemented recipe list

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('recipes/list', recipes, name="recipes"),
+    path('recipe/<int:id>/', recipe, name='recipe')
+
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,15 +1,84 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+CTX = {
+        "recipes": [{
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2 cup"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+} 
 
 def index(request):
     return HttpResponse('Welcome to the Ledger App!')
 
-def recipe_list(request):
-    ctx = [
-        
-    ]
+def recipes(request):
+
+    return render(request, "ledger/recipes.html", CTX)
+
+def recipe(request, id):
+
+    ctx = {"recipe": CTX["recipes"][id - 1]}
+
+    return render(request, "ledger/recipe.html", ctx)
+    
+
 
 
 # Create your views here.
