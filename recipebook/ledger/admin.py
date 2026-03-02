0bad5f6994eef4da729f6cd091dcfd3294a6fa5f
mikaellacaluag <mikaella.caluag@student.ateneo.edu>
2026-03-02 17:35:06 +0800

    Fixed templates

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -4,36 +4,39 @@ from django.contrib.auth.models import User
 
 from .models import Recipe, RecipeIngredient, Profile
 
+
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
 
+
 class UserAdmin(BaseUserAdmin):
-    inlines = [ProfileInline,]
+    inlines = [
+        ProfileInline,
+    ]
+
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline, ]
+    inlines = [
+        RecipeIngredientInline,
+    ]
 
 
 class RecipeIngredientAdmin(admin.ModelAdmin):
     model = RecipeIngredient
 
-    search_fields = ('recipe', )
+    search_fields = ("recipe",)
 
-    list_display = ('ingredient', 'quantity', 'recipe')
+    list_display = ("ingredient", "quantity", "recipe")
 
-    list_filter = ('recipe', )
+    list_filter = ("recipe",)
 
-    fieldsets = [
-        ('Details',{
-            'fields': [
-                ('ingredient', 'quantity'), 'recipe'
-            ]
-        })
-    ]
+    fieldsets = [("Details", {"fields": [("ingredient", "quantity"), "recipe"]})]
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,74 +3,68 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     bio = models.TextField(
         validators=[
-            MinLengthValidator(255, 'the field must contain at least 255 characters')
+            MinLengthValidator(255, "the field must contain at least 255 characters")
         ]
     )
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe-list')
-    
+        return reverse("ledger:ingredient_detail", args=[self.pk])
+
     class Meta:
-        ordering = ['name']
-        verbose_name = 'ingredient'
-        verbose_name_plural = 'ingredients'
+        ordering = ["name"]
+        verbose_name = "ingredient"
+        verbose_name_plural = "ingredients"
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
-    author =models.ForeignKey(
-        Profile, 
-        on_delete=models.CASCADE,
-        related_name = "recipes"
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="recipes"
     )
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
-        return reverse('ledger:recipe-detail', args=[str(self.pk)])
-    
+        return reverse("ledger:recipe-detail", args=[str(self.pk)])
+
     class Meta:
-        ordering = ['name']
-        verbose_name = 'recipe'
-        verbose_name_plural = 'recipes'
+        ordering = ["name"]
+        verbosename = "recipe"
+        verbose_name_plural = "recipes"
+
 
 class RecipeIngredient(models.Model):
     quantity = models.IntegerField()
 
     ingredient = models.ForeignKey(
-        Ingredient, 
-        on_delete=models.CASCADE, 
-        related_name="recipe"
+        Ingredient, on_delete=models.CASCADE, related_name="recipe"
     )
 
     recipe = models.ForeignKey(
-        Recipe, 
-        on_delete=models.CASCADE, 
-        related_name = "ingredients"
+        Recipe, on_delete=models.CASCADE, related_name="ingredients"
     )
 
     def __str__(self):
-        return '{} {} for {}'.format(self.quantity, self.ingredient, self.recipe)
-    
-    class Meta:
-        ordering = ['recipe', 'ingredient']
-        verbose_name = 'recipe ingredient'
-        verbose_name_plural = 'recipe ingredients'
-        unique_together = ('recipe', 'ingredient')
-    
-    
+        return "{} {} for {}".format(self.quantity, self.ingredient, self.recipe)
 
-    
+    class Meta:
+        ordering = ["recipe", "ingredient"]
+        verbose_name = "recipe ingredient"
+        verbose_name_plural = "recipe ingredients"
+        unique_together = ("recipe", "ingredient")
