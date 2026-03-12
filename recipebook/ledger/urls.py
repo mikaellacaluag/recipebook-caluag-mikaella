a1798a3e9c93718097fee205086206e5ef34cc41
mikaellacaluag <mikaella.caluag@student.ateneo.edu>
2026-03-12 16:15:44 +0800

    Added add_images.html and modify app-level urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('recipes/list', recipes, name="recipe-list"),
     path('recipe/<int:id>/', recipe, name='recipe-detail'),
+    path('recipe/<int:id>/add-image/', recipe_forms, name="recipe_forms"),
 ]
 
 app_name = "ledger"
